41bba1a5a1f84ca41057281ccef1d4802ac2a1bc
Mohammed Naji <m.naji1512@gmail.com>
2023-09-07 13:08:23 +0300

    admin dashboard layout

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.master')
+
+@section('content')
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+@endsection
+
+@section('title', 'Dashboard')
---
+++ routes/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+});
